558c95e80a9febf647b6ba1d70552887c83f3887
Andrew Derkach <junglnet@gmail.com>
2024-02-11 13:23:14 +0300

    Add db

---
+++ db/SQLQuery2.sql
@@ -0,0 +1,9 @@
+INSERT INTO [schoolscheldule].[dbo].[schelduletable]
+	([GradeNumber]
+      ,[GradeLatter]
+      ,[Cabinet]
+      ,[TeacherName]
+      ,[LessonName]
+      ,[Weekday]
+      ,[LessonOrder])
+VALUES (10, 'Ю', '304', 'Иванов Иван Иванович','Математика', 2, 1);
